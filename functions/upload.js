5d72c12c32c711449f6c7ef5704f7b9281072a5e
Carlo <carlo.schiesaro@gmail.com>
2020-12-09 18:31:59 +0000

    Added cors to upload function

--- functions/upload.js
+++ functions/upload.js
@@ -42,6 +42,12 @@ exports.handler = function (event, context, callback) {
 
   callback(null, {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': 'https://www.zwiftworkout.com',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({
       uploadURL: uploadURL
     }),
