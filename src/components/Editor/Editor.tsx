1eb109574e0a7f73f9778b7831caee2cdd938efb
Rene Saarsoo <nene@triin.net>
2020-12-22 21:59:55 +0200

    Extract TimeAxis component

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -10,6 +10,7 @@ import Popup from '../Popup/Popup'
 import Footer from '../Footer/Footer'
 import Workouts from '../Workouts/Workouts'
 import Checkbox from './Checkbox'
+import TimeAxis from './TimeAxis'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faArrowRight, faArrowLeft, faFile, faSave, faUpload, faDownload, faComment, faBicycle, faCopy, faClock, faShareAlt, faTimesCircle, faList, faBiking, faRunning, faRuler } from '@fortawesome/free-solid-svg-icons'
 import { ReactComponent as WarmdownLogo } from '../../assets/warmdown.svg'
@@ -1229,45 +1230,7 @@ const Editor = ({ match }: RouteComponentProps<TParams>) => {
           </div>
 
           {durationType === 'time' ?
-          <div className='timeline' style={{width: segmentsWidth}}>
-            <span>0:00</span>
-            <span>0:10</span>
-            <span>0:20</span>
-            <span>0:30</span>
-            <span>0:40</span>
-            <span>0:50</span>
-            <span>1:00</span>
-            <span>1:10</span>
-            <span>1:20</span>
-            <span>1:30</span>
-            <span>1:40</span>
-            <span>1:50</span>
-            <span>2:00</span>
-            <span>2:10</span>
-            <span>2:20</span>
-            <span>2:30</span>
-            <span>2:40</span>
-            <span>2:50</span>
-            <span>3:00</span>
-            <span>3:10</span>
-            <span>3:20</span>
-            <span>3:30</span>
-            <span>3:40</span>
-            <span>3:50</span>
-            <span>4:00</span>
-            <span>4:10</span>
-            <span>4:20</span>
-            <span>4:30</span>
-            <span>4:40</span>
-            <span>4:50</span>
-            <span>5:00</span>
-            <span>5:10</span>
-            <span>5:20</span>
-            <span>5:30</span>
-            <span>5:40</span>
-            <span>5:50</span>
-            <span>6:00</span>
-          </div>
+          <TimeAxis width={segmentsWidth} />
           :        
           <div className='timeline run' style={{width: segmentsWidth}}>            
             {[...Array(44)].map((e,i) => <span key={i}>{i}K</span>)}            
---
+++ src/components/Editor/TimeAxis.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TimeAxis = ({ width }: { width: number }) => (
+  <div className='timeline' style={{ width }}>
+    <span>0:00</span>
+    <span>0:10</span>
+    <span>0:20</span>
+    <span>0:30</span>
+    <span>0:40</span>
+    <span>0:50</span>
+    <span>1:00</span>
+    <span>1:10</span>
+    <span>1:20</span>
+    <span>1:30</span>
+    <span>1:40</span>
+    <span>1:50</span>
+    <span>2:00</span>
+    <span>2:10</span>
+    <span>2:20</span>
+    <span>2:30</span>
+    <span>2:40</span>
+    <span>2:50</span>
+    <span>3:00</span>
+    <span>3:10</span>
+    <span>3:20</span>
+    <span>3:30</span>
+    <span>3:40</span>
+    <span>3:50</span>
+    <span>4:00</span>
+    <span>4:10</span>
+    <span>4:20</span>
+    <span>4:30</span>
+    <span>4:40</span>
+    <span>4:50</span>
+    <span>5:00</span>
+    <span>5:10</span>
+    <span>5:20</span>
+    <span>5:30</span>
+    <span>5:40</span>
+    <span>5:50</span>
+    <span>6:00</span>
+  </div>
+);
+
+export default TimeAxis;
