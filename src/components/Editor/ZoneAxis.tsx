40ea7d74d713ee9273c8395d21e42498f1e2b96c
Rene Saarsoo <nene@triin.net>
2020-12-22 22:54:42 +0200

    Replace hard-coded zones with code

--- src/components/Editor/ZoneAxis.tsx
+++ src/components/Editor/ZoneAxis.tsx
@@ -4,12 +4,8 @@ import "./ZoneAxis.css";
 
 const ZoneAxis = () => (
   <div className='zone-axis'>
-    <div style={{ height: 250 * Zones.Z6.max }}>Z6</div>
-    <div style={{ height: 250 * Zones.Z5.max }}>Z5</div>
-    <div style={{ height: 250 * Zones.Z4.max }}>Z4</div>
-    <div style={{ height: 250 * Zones.Z3.max }}>Z3</div>
-    <div style={{ height: 250 * Zones.Z2.max }}>Z2</div>
-    <div style={{ height: 250 * Zones.Z1.max }}>Z1</div>
+    {Object.entries(Zones).reverse().map(([name, zone]) =>
+      (<div style={{ height: 250 * zone.max }}>{name}</div>))}
   </div>
 );
 
