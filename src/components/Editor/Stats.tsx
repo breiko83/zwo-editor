0ec8488517d13ba5655571728e85d7bd68d9661f
Rene Saarsoo <nene@triin.net>
2021-01-02 15:08:45 +0200

    Move getStressScore inside Stats component

--- src/components/Editor/Stats.tsx
+++ src/components/Editor/Stats.tsx
@@ -18,10 +18,43 @@ const Stats: React.FC<StatsProps> = ({ intervals, ftp }) => {
       </div>
       <div className="form-input">
         <label>TSS</label>
-        <input className="textInput" value={helpers.getStressScore(intervals, ftp)} disabled />
+        <input className="textInput" value={getStressScore(intervals, ftp)} disabled />
       </div>
     </>
   );
 };
+
+function getStressScore(intervals: Interval[], ftp: number): string {
+  // TSS = [(sec x NP x IF)/(FTP x 3600)] x 100
+  let tss = 0
+
+  intervals.map((interval) => {
+    if (interval.type === 'steady' && interval.power) {
+      const np = interval.power * ftp
+      const iff = interval.power
+
+      tss += (interval.duration * np * iff)
+    }
+    if (interval.type === 'ramp' && interval.startPower && interval.endPower) {
+      const np = (interval.startPower + interval.endPower) / 2 * ftp
+      const iff = (interval.startPower + interval.endPower) / 2
+
+      tss += (interval.duration * np * iff)
+    }
+    if (interval.type === 'repetition' && interval.onPower && interval.offPower && interval.repeat && interval.onDuration && interval.offDuration) {
+      const npOn = (interval.onPower * ftp)
+      const iffOn = interval.onPower
+
+      tss += (interval.onDuration * interval.repeat * npOn * iffOn)
+
+      const npOff = (interval.offPower * ftp)
+      const iffOff = interval.offPower
+
+      tss += (interval.offDuration * interval.repeat * npOff * iffOff)
+    }
+    return false;
+  })
+  return ((tss / (ftp * 3600)) * 100).toFixed(0);
+}
 
 export default Stats;
--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,6 +1,5 @@
 import moment from 'moment'
 import 'moment-duration-format'
-import { ZoneColor, Zones } from './Zones'
 import { Interval } from '../types/Interval'
 
 const helpers = {
@@ -23,41 +22,6 @@ const helpers = {
 
     return duration
   },
-
-  getStressScore: function (intervals: Interval[], ftp: number): string {
-
-    // TSS = [(sec x NP x IF)/(FTP x 3600)] x 100
-    var tss = 0
-
-    intervals.map((interval) => {
-      if (interval.type === 'steady' && interval.power) {
-        const np = interval.power * ftp
-        const iff = interval.power
-
-        tss += (interval.duration * np * iff)
-      }
-      if (interval.type === 'ramp' && interval.startPower && interval.endPower) {
-        const np = (interval.startPower + interval.endPower) / 2 * ftp
-        const iff = (interval.startPower + interval.endPower) / 2
-
-        tss += (interval.duration * np * iff)
-      }
-      if (interval.type === 'repetition' && interval.onPower && interval.offPower && interval.repeat && interval.onDuration && interval.offDuration) {
-        const npOn = (interval.onPower * ftp)
-        const iffOn = interval.onPower
-
-        tss += (interval.onDuration * interval.repeat * npOn * iffOn)
-
-        const npOff = (interval.offPower * ftp)
-        const iffOff = interval.offPower
-
-        tss += (interval.offDuration * interval.repeat * npOff * iffOff)
-      }
-      return false;
-    })
-    return ((tss / (ftp * 3600)) * 100).toFixed(0);
-  },
-
 
   getTimeinSeconds: function (time: string): number {
     //convert time 01:00:00 to seconds 3600
