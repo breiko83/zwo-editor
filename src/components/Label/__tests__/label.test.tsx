6b0072feef9f05b4d99e6182bce9ab8bc20c6f84
Rene Saarsoo <nene@triin.net>
2021-01-03 17:42:11 +0200

    Additional tests for Label component

--- src/components/Label/__tests__/label.test.tsx
+++ src/components/Label/__tests__/label.test.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import Label from '../Label';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect'
+import { PaceType } from '../../../types/PaceType';
 
-test('Label renders correctly', () => {
-  const component = renderer.create(
-    <Label duration={100} power={250} weight={75} sportType="bike" cadence={0} onCadenceChange={() => {}} />
-  )
+describe('<Label>', () => {
+  test('for cycling, renders: duration, power, w/kg, %FTP, cadence', () => {
+    const component = renderer.create(
+      <Label duration={100} power={250} weight={75} ftp={200} sportType="bike" cadence={0} onCadenceChange={() => {}} />
+    );
+    expect(component).toMatchSnapshot();
+  });
 
-  expect(component).toMatchSnapshot();
-})
+  test('for cycling ramp, renders: duration, power-range, %FTP-range, cadence', () => {
+    const component = renderer.create(
+      <Label duration={100} powerStart={100} powerEnd={200} weight={75} ftp={200} sportType="bike" cadence={0} onCadenceChange={() => {}} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  test('for running, renders: duration, %FTP, pace type, cadence', () => {
+    const component = renderer.create(
+      <Label duration={100} power={250} ftp={200} weight={75} pace={PaceType.tenKm} sportType="run" cadence={0} onCadenceChange={() => {}} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  test('for running ramp, renders: duration, %FTP-range, pace type, cadence', () => {
+    const component = renderer.create(
+      <Label duration={100} powerStart={100} powerEnd={200} ftp={200} weight={75} pace={PaceType.tenKm} sportType="run" cadence={0} onCadenceChange={() => {}} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
