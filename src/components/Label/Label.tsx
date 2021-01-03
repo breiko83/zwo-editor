5f63bb338292112ac393fa349a726bd2c74547a7
Rene Saarsoo <nene@triin.net>
2021-01-03 18:09:32 +0200

    Split rendering of bike / run data inside Label

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -19,61 +19,74 @@ interface LabelProps {
 }
 
 const Label = (props: LabelProps) => {
-  const paces = ["1M", "5K", "10K", "HM", "M"]  
-
   return (
     <div className='label'>
       <div>
         <FontAwesomeIcon icon={faClock} fixedWidth /> {helpers.formatDuration(props.duration)}
       </div>
-      {props.power && props.sportType === "bike" &&
+      {props.sportType === "bike" ? <BikeData {...props} /> : <RunData {...props} />}
+    </div>
+  );
+};
+
+function BikeData(props: LabelProps) {
+  return (
+    <>
+      {props.power &&
         <div>
           <FontAwesomeIcon icon={faBolt} fixedWidth /> {props.power}W
         </div>
       }
-      {props.powerStart && props.powerEnd && props.sportType === "bike" &&
+      {props.powerStart && props.powerEnd &&
         <div>
           <FontAwesomeIcon icon={faBolt} fixedWidth /> {props.powerStart}W - {props.powerEnd}W
         </div>
       }
-      {props.weight && props.power && props.ftp && props.sportType === "bike" &&
+      {props.weight && props.power && props.ftp &&
         <div>
           {(props.power / props.weight).toFixed(1)}W/Kg &middot; {(props.power / props.ftp * 100).toFixed(0)}% FTP
         </div>
       }
-      {props.powerStart && props.powerEnd && props.ftp && props.sportType === "bike" &&
+      {props.powerStart && props.powerEnd && props.ftp &&
         <div>
           {(props.powerStart / props.ftp * 100).toFixed(0)}% FTP - {(props.powerEnd / props.ftp * 100).toFixed(0)}% FTP
         </div>
       }
-      {props.power && props.ftp && props.pace !== undefined && props.sportType === "run" &&
+      <div className="cadence-row">
+        <label className="cadenceLabel">Cadence</label>
+        <input
+          type="number"
+          min="40"
+          max="150"
+          step="5"
+          name="cadence"
+          value={props.cadence || ''}
+          onChange={(e) => {props.onCadenceChange(parseInt(e.target.value))}}
+          onClick={(e)=> {e.stopPropagation()}}
+          className="textField cadence"
+        />
+      </div>
+    </>
+  );
+}
+
+function RunData(props: LabelProps) {
+  const paces = ["1M", "5K", "10K", "HM", "M"];
+
+  return (
+    <>
+      {props.power && props.ftp && props.pace !== undefined &&
         <div>
           {(props.power / props.ftp * 100).toFixed(1).replace(/[.]0$/, "")}% {paces[props.pace || 0]} pace
         </div>
       }
-      {props.powerStart && props.powerEnd && props.ftp && props.pace !== undefined && props.sportType === "run" &&
+      {props.powerStart && props.powerEnd && props.ftp && props.pace !== undefined &&
         <div>
           {(props.powerStart / props.ftp * 100).toFixed(0)}% to {(props.powerEnd / props.ftp * 100).toFixed(0)}% {paces[props.pace || 0]} pace
         </div>
       }
-      {props.sportType === "bike" &&
-        <div className="cadence-row">
-          <label className="cadenceLabel">Cadence</label>
-          <input
-            type="number"
-            min="40"
-            max="150"
-            step="5"
-            name="cadence"
-            value={props.cadence || ''}
-            onChange={(e) => {props.onCadenceChange(parseInt(e.target.value))}}
-            onClick={(e)=> {e.stopPropagation()}}
-            className="textField cadence"
-          />
-        </div>
-      }
-    </div>
-  )
+    </>
+  );
 }
 
 export default Label
