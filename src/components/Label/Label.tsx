c1723adc9e5255e1e4a61e73195eb5505ed033ae
Carlo <carlo.schiesaro@gmail.com>
2021-01-09 11:40:30 +0000

    Removed cadence for running workouts

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -48,11 +48,13 @@ const Label = (props: { sportType: string, duration: string, distance?: number, 
         <div>
           {(props.powerStart / props.ftp * 100).toFixed(0)}% to {(props.powerEnd / props.ftp * 100).toFixed(0)}% {paces[props.pace || 0]} pace
         </div>
-      }      
-      <div className="cadence-row">
-        <label className="cadenceLabel">Cadence</label>
-        <input type="number" min="40" max="150" step="5" name="cadence" value={props.cadence || ''} onChange={(e) => {if (props.setCadence) props.setCadence(parseInt(e.target.value))}} onClick={(e)=> {e.stopPropagation()}} className="textField cadence" />
-      </div>
+      }    
+      {props.sportType === "bike" &&
+        <div className="cadence-row">
+          <label className="cadenceLabel">Cadence</label>
+          <input type="number" min="40" max="150" step="5" name="cadence" value={props.cadence || ''} onChange={(e) => {if (props.setCadence) props.setCadence(parseInt(e.target.value))}} onClick={(e)=> {e.stopPropagation()}} className="textField cadence" />
+        </div>
+      }
     </div>
   )
 }
