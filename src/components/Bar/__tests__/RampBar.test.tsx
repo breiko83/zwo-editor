fdf945ecd85871601845e87d007659b3208768bf
Rene Saarsoo <nene@triin.net>
2021-01-11 18:24:39 +0200

    Test RampBar with & without Label

--- src/components/Bar/__tests__/RampBar.test.tsx
+++ src/components/Bar/__tests__/RampBar.test.tsx
@@ -7,23 +7,55 @@ import intervalFactory from '../../../interval/intervalFactory';
 import createMode from '../../../modes/createMode';
 import { Duration } from '../../../types/Length';
 
-test('RampBar renders correctly', () => {
-  const mode = createMode({sportType: "bike", ftp: 250, weight: 75, runningTimes: [], lengthType: "time"});
-  const interval = intervalFactory.ramp({
-    length: new Duration(50),
-    startIntensity: Zones.Z2.min,
-    endIntensity: Zones.Z4.min,
-  }, mode);
+const MockReact = React;
 
-  const component = renderer.create(
-    <RampBar
-      interval={interval}
-      mode={mode}
-      selected={false}
-      onChange={() => { }}
-      onClick={() => { }}
-    />
-  )
+jest.mock('../../Label/Label', () => (props: any) =>
+  MockReact.createElement("Label", props));
 
-  expect(component).toMatchSnapshot();
-})
+jest.mock('re-resizable', () => ({
+  Resizable: (props: any) => MockReact.createElement("Resizable", props),
+}));
+
+describe('<SteadyBar>', () => {
+  it('renders', () => {
+    const mode = createMode({sportType: "bike", ftp: 250, weight: 75, runningTimes: [], lengthType: "time"});
+    const interval = intervalFactory.ramp({
+      length: new Duration(50),
+      startIntensity: Zones.Z2.min,
+      endIntensity: Zones.Z4.min,
+    }, mode);
+
+    const component = renderer.create(
+      <RampBar
+        interval={interval}
+        mode={mode}
+        selected={false}
+        onChange={() => { }}
+        onClick={() => { }}
+      />
+    )
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders with Label when selected', () => {
+    const mode = createMode({sportType: "bike", ftp: 250, weight: 75, runningTimes: [], lengthType: "time"});
+    const interval = intervalFactory.ramp({
+      length: new Duration(50),
+      startIntensity: Zones.Z2.min,
+      endIntensity: Zones.Z4.min,
+    }, mode);
+
+    const component = renderer.create(
+      <RampBar
+        interval={interval}
+        mode={mode}
+        selected={true}
+        onChange={() => { }}
+        onClick={() => { }}
+      />
+    )
+
+    expect(component).toMatchSnapshot();
+  });
+});
