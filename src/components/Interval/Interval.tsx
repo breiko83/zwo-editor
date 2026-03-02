62859f836fb48a408686686d338991adee828314
Carlo Schiesaro <carlo.schiesaro@gmail.com>
2026-03-02 19:22:25 +0000

    fix: specify type for bars in useEffect

--- src/components/Interval/Interval.tsx
+++ src/components/Interval/Interval.tsx
@@ -37,7 +37,7 @@ const Interval = (props: {
   const [offLength, setOffLength] = useState(props.offLength);
 
   useEffect(() => {
-    const bars = [];
+    const bars: BarType[] = [];
 
     for (var i = 0; i < nIntervals; i++) {
       bars.push({
